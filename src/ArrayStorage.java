bf0c366391b903a4694797e43eb714ace325d874
Evgeniy <sutyaginev@gmail.com>
2024-06-25 15:34:58 +0500

    Исправлены замечания: 1-добавлено поле size. 2-убран this. 3-исправлен метод delete(String uuid).

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,70 +5,55 @@ import java.util.Objects;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size;
 
     void clear() {
-        int storageSize = this.size();
-        for (int i = 0; i < storageSize; i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+
+        size = 0;
     }
 
     void save(Resume r) {
-        int storageSize = this.size();
-        if (storageSize < 10000) {
-            storage[storageSize] = r;
+        if (size < 10000) {
+            storage[size] = r;
         }
+
+        size++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < this.size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (Objects.equals(storage[i].uuid, uuid)) {
                 return storage[i];
             }
         }
+
         return null;
     }
 
     void delete(String uuid) {
-        int deletedResumeIndex = -1;
-        int storageSize = this.size();
-
-        for (int i = 0; i < storageSize; i++) {
+        for (int i = 0; i < size; i++) {
             if (Objects.equals(storage[i].uuid, uuid)) {
-                deletedResumeIndex = i;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
             }
         }
-
-        if (deletedResumeIndex == -1) {
-            return;
-        }
-
-        for (int i = deletedResumeIndex + 1; i < storageSize; i++) {
-            storage[i - 1] = storage[i];
-        }
-
-        storage[storageSize - 1] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int storageSize = this.size();
-        Resume[] resumes = new Resume[storageSize];
-
-        System.arraycopy(storage, 0, resumes, 0, storageSize);
+        Resume[] resumes = new Resume[size];
+        System.arraycopy(storage, 0, resumes, 0, size);
 
         return resumes;
     }
 
     int size() {
-        int size = 0;
-
-        while (storage[size] != null) {
-            size++;
-        }
-
         return size;
     }
 }
