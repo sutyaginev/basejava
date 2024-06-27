c9d895dd512d9a6113461b2c1aad5b0ce6f79774
Evgeniy <sutyaginev@gmail.com>
2024-06-27 09:53:22 +0500

    Классы размещены по пакетам. Добавлен геттер и сеттер для uuid. Добавлены модификаторы доступа для полей и методов.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,13 +1,17 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 import java.util.Objects;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size;
+    private Resume[] storage = new Resume[10000];
+    private int size;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
@@ -15,7 +19,7 @@ public class ArrayStorage {
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         if (size < 10000) {
             storage[size] = r;
         }
@@ -23,9 +27,9 @@ public class ArrayStorage {
         size++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(storage[i].uuid, uuid)) {
+            if (Objects.equals(storage[i].getUuid(), uuid)) {
                 return storage[i];
             }
         }
@@ -33,9 +37,9 @@ public class ArrayStorage {
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(storage[i].uuid, uuid)) {
+            if (Objects.equals(storage[i].getUuid(), uuid)) {
                 storage[i] = storage[size - 1];
                 storage[size - 1] = null;
                 size--;
@@ -46,14 +50,14 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] resumes = new Resume[size];
         System.arraycopy(storage, 0, resumes, 0, size);
 
         return resumes;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }
