80c8ce22a8a29101ddbf7dff67f9dc5d517249f9
Evgeniy <sutyaginev@gmail.com>
2024-07-23 23:48:25 +0500

    Реализован AllStorageTest

---
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -0,0 +1,15 @@
+package com.urise.webapp.storage;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapUuidStorageTest.class,
+        MapResumeStorageTest.class
+})
+public class AllStorageTest {
+}
