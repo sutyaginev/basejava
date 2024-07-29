4abb6c970d6a07c9861f5b1f6777344a763953d9
Evgeniy <sutyaginev@gmail.com>
2024-07-29 11:56:47 +0500

    Реализовано логирование

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -6,27 +6,34 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
 
     private static final Comparator<Resume> fullNameUuidComparator = Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
+//    protected final Logger log = Logger.getLogger(getClass().getName());
+private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     public final void update(Resume resume) {
+        LOG.info("Update " + resume);
         SK searchKey = getExistingSearchKey(resume.getUuid());
         doUpdate(searchKey, resume);
     }
 
     public final void save(Resume resume) {
+        LOG.info("Save " + resume);
         SK searchKey = getNotExistingSearchKey(resume.getUuid());
         doSave(searchKey, resume);
     }
 
     public final Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK searchKey = getExistingSearchKey(uuid);
         return doGet(searchKey);
     }
 
     public final void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK searchKey = getExistingSearchKey(uuid);
         doDelete(searchKey);
     }
@@ -38,6 +45,7 @@ public abstract class AbstractStorage<SK> implements Storage {
             return searchKey;
         }
 
+        LOG.warning("Resume " + uuid + " not exist");
         throw new NotExistStorageException(uuid);
     }
 
@@ -48,11 +56,13 @@ public abstract class AbstractStorage<SK> implements Storage {
             return searchKey;
         }
 
+        LOG.warning("Resume " + uuid + " already exist");
         throw new ExistStorageException(uuid);
     }
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> resumes = doCopyAll();
         resumes.sort(fullNameUuidComparator);
         return resumes;
