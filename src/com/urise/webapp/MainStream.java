92e32e4e3e4bce961c51d8106c7d20436a149874
Evgeniy <sutyaginev@gmail.com>
2024-10-13 18:35:42 +0500

    Выполнено HW12

---
+++ src/com/urise/webapp/MainStream.java
@@ -0,0 +1,29 @@
+package com.urise.webapp;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainStream {
+
+    public static void main(String[] args) {
+        System.out.println(minValue(new int[]{1,2,3,3,2,3}));
+        System.out.println(minValue(new int[]{9, 8}));
+
+        System.out.println(oddOrEven(Arrays.asList(1,2,3,3,2,3)));
+        System.out.println(oddOrEven(Arrays.asList(9 ,8)));
+    }
+
+    private static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (number, digit) -> (number * 10 + digit));
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        return integers.stream()
+                .filter(num -> (integers.stream().mapToInt(Integer::intValue).sum() % 2 == 0) == (num % 2 != 0))
+                .collect(Collectors.toList());
+    }
+}
